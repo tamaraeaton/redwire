7eb215ef12a2e13555d3cd8ae7b0b252bee050d8
tamaraeaton <teaton@banyanlabs.io>
2021-04-22 15:07:59 -0700

    #73 Complete

--- src/routes/stacks.js
+++ src/routes/stacks.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Platform, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Colors, LogoText } from '../utils/tools';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native'
 
 import VideosScreen from '../components/home/videos';
 import VideoScreen from '../components/home/videos/video';
@@ -9,22 +13,60 @@ import ArticleScreen from '../components/home/articles/article';
 
 export const Stack = createStackNavigator();
 
+const LeftIcon = () => {
+    const navigation = useNavigation();
+    return (
+        <View style={{ margin: 10 }}>
+            <Icon
+                name="menufold"
+                type="antdesign"
+                color={Colors.white}
+                onPress={() => navigation.openDrawer()}
+            />
+        </View>
+    )
+}
+
+export const screenOptions = {
+    headerTitleAlign: 'center',
+    headerTintColor: Colors.red,
+    headerStyle: {
+        backgroundColor: Colors.black,
+        borderBottomWidth: 6,
+        borderBottomColor: Colors.red, height: Platform.OS === 'ios' ? 110 : 60
+    },
+    headerTitle: () => <LogoText style={{ fontSize: 25 }} />
+}
+
 export const VideosStack = () => {
     return (
-    <Stack.Navigator
-        initialRouteName="Videos_screen"
-    >
-        <Stack.Screen name="Videos_screen" component={VideosScreen}/>
-        <Stack.Screen name="Video_screen" component={VideoScreen}/>
-    </Stack.Navigator>
-    )}
+        <Stack.Navigator
+            screenOptions={{
+                ...screenOptions
+            }}
+            initialRouteName="Videos_screen"
+        >
+            <Stack.Screen name="Videos_screen" component={VideosScreen} options={{
+                headerLeft: () => <LeftIcon />
+            }} />
+            <Stack.Screen name="Video_screen" component={VideoScreen} />
+        </Stack.Navigator>
+    )
+}
 
 export const HomeStack = () => {
     return (
-    <Stack.Navigator
-        initialRouteName="Home_screen"
-    >
-        <Stack.Screen name="Home_screen" component={HomeScreen}/>
-        <Stack.Screen name="Article_screen" component={ArticleScreen}/>
-    </Stack.Navigator>
-    )}
+        <Stack.Navigator
+            screenOptions={{
+                ...screenOptions
+            }}
+            initialRouteName="Home_screen"
+        >
+            <Stack.Screen name="Home_screen" component={HomeScreen}
+                options={{
+                    headerLeft: () => <LeftIcon />
+                }} />
+            <Stack.Screen name="Article_screen" component={ArticleScreen} />
+        </Stack.Navigator>
+    )
+}
