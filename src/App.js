546e0d356a31ca21a4b29962e29c566a4fb9a696
tamaraeaton <teaton@banyanlabs.io>
2021-04-22 08:25:22 -0700

    #69 Completed

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { connect } from 'react-redux';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+import VideosScreen from './components/home/videos';
+import HomeScreen from './components/home/articles';
+import AuthScreen from './components/auth';
+
+const MainDrawer = () => {
+  return (
+  <Drawer.Navigator>
+    <Drawer.Screen name="Home" component={HomeScreen}/>
+    <Drawer.Screen name="Videos" component={VideosScreen}/>
+  </Drawer.Navigator>
+  )}
+
+class App extends Component {
+  render(){
+    return(
+      <NavigationContainer>
+        <Stack.Navigator>
+          {this.props.auth.isAuth ? 
+           <>
+           <Stack.Screen
+            name="Main"
+            component={ MainDrawer }
+           />
+           </>
+          :
+            <Stack.Screen 
+              name="AuthScreen"
+              component={AuthScreen}
+            />
+          }
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
+}
+
+const mapStateToProps = state => ({auth: state.auth})
+export default connect(mapStateToProps)(App);
+
+
---
+++ src/components/home/videos/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const VideosScreen = () => {
+    return (
+        <View>
+            <Text>Videos</Text>
+        </View>
+    )
+}
+
+export default VideosScreen;
