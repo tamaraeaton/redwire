0f6474a9d3af91d32a64dd8aba235cbd30325868
tamaraeaton <teaton@banyanlabs.io>
2021-04-23 13:09:12 -0700

    #76 Can not get {Image} or {Tile} to work with react-native-elements

--- src/components/home/articles/article/index.js
+++ src/components/home/articles/article/index.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { 
+import {
     View, Text, 
-    ScrollView, ActivityIndicator, Image
+    ScrollView, ActivityIndicator
 } from 'react-native';
-import ContentShow from '../../../../utils/contentShow';
+import { Image } from 'react-native-elements';
 
+import ContentShow from '../../../../utils/contentShow';
+const image = require('./imageTest.jpg')
 const ArticleScreen = () => {
     return (
         <ScrollView>
             <View>
-                <Image 
-                    source={{uri:'https://picsum.photos/200/300.jpg'}}
-                    style={{width:'100%', height:200}}
-                    PlaceholderContent={<ActivityIndicator/>}
+                <Image
+                    source={{ uri: 'https://picsum.photos/200/300.jpg' }}
+                    style={{ width: '100%', height: 200 }}
+                    PlaceholderContent={<ActivityIndicator />}
                 />
-                <ContentShow/>
+                <Image
+                    containerStyle={{}}
+                    onLongPress={() => console.log("onLongPress()")}
+                    onPress={() => console.log("onPress()")}
+                    placeholderStyle={{}}
+                    transitionDuration={1000}
+                    source={{
+                        uri:
+                            "https://user-images.githubusercontent.com/5962998/65694309-a825f000-e043-11e9-8382-db0dba0851e3.png"
+                    }}
+                    style={{ width: 200, height: 200 }}
+                />
+                <ContentShow />
             </View>
         </ScrollView>
     )
 }
-
 export default ArticleScreen;
--- src/components/home/videos/index.js
+++ src/components/home/videos/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, ScrollView, ActivityIndicator } from 'react-native';
+import { Tile } from 'react-native-elements';
 
-const VideosScreen = ({navigation}) => {
+const VideosScreen = ({ navigation }) => {
+
+    const renderVideos = () => {
+        <Tile
+            imageSrc={'https://picsum.photos/200/300'}
+            title={'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'}
+        />
+    }
+
     return (
-        <View>
-            <Text>Videos</Text>
-            <Button 
+        <ScrollView>
+            <View>
+                <Text>Videos</Text>
+                <Tile
+                    activeOpacity={0.5}
+                    caption="Tile caption"
+                    captionStyle={{}}
+                    containerStyle={{}}
+                    contentContainerStyle={{}}
+                    height={300}
+                    iconContainerStyle={{}}
+                    imageContainerStyle={{}}
+                    imageProps={{}}
+                    imageSrc="SampleImage"
+                    onPress={() => console.log("imageProps()")}
+                    overlayContainerStyle={{}}
+                    titleStyle={{}}
+                    width={400}
+                />
+                {renderVideos()}
+            </View>
+            {/* <Button 
                 title="see video"
                 onPress={()=> navigation.navigate('Video_screen')}
-            />
-        </View>
+            /> */}
+        </ScrollView>
     )
 }
 
