8c86c87b39593e58e460646719664a7503c17c1c
tamaraeaton <teaton@banyanlabs.io>
2021-04-23 07:50:16 -0700

    #74 Complete

--- src/components/home/articles/index.js
+++ src/components/home/articles/index.js
@@ -1,16 +1,58 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { 
+    View, Text, 
+    Button, ScrollView, 
+    TouchableOpacity, StyleSheet 
+} from 'react-native';
+import { Card } from 'react-native-elements';
 
-const HomeScreen = ({navigation}) => {
+
+const HomeScreen = ({ navigation }) => {
+
+    const renderCard = () => {
+        return (
+        <TouchableOpacity
+            onLongPress={()=>navigation.navigate('Article_screen'),{
+                id:'1234',
+                postData: {title:'lkjlk', content: ''}
+            }}
+        >
+            <Card>
+                <Card.Title style={styles.cardTitle}>
+                    <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Text>
+                </Card.Title>
+           
+            <Card.Divider/>
+            <Text style={styles.cardText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+            optio, eaque rerum! Provident similique accusantium nemo autem.
+            </Text>
+            </Card>
+        </TouchableOpacity>
+        )}
+
     return (
-        <View>
-            <Text>Home Screen</Text>
-            <Button 
-                title="see article"
-                onPress={()=> navigation.navigate('Article_screen')}
-            />
-        </View>
+        <ScrollView>
+            {renderCard()}
+            {renderCard()}
+            {renderCard()}
+            {renderCard()}
+            {renderCard()}
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    cardTitle:{
+        fontSize:20,
+        textAlign:'left'
+    },
+    cardText:{
+        marginBottom:10,
+        marginTop:10
+    }
+})
 
 export default HomeScreen;
